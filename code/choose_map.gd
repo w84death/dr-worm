5f83fb0f47557b8e626ee440ce9364bb12d8bbb3
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2018-03-05 14:19:12 +0100

    map chooser

--- code/choose_map.gd
+++ code/choose_map.gd
@@ -4,19 +4,19 @@
 # http://p1x.in | krzysztofjankowski.com
 
 var menu_selected = 0
-const MENU_START = 0
-const MENU_HISCORE = 1
-const MENU_EXIT = 2
+const MENU_GRASS = 0
+const MENU_WATER = 1
+const MENU_BACK = 2
 
 func _ready():
 	self.grab_focus()
-
+	
 func _on_menu_input_event( ev ):
-	if Input.is_key_pressed(KEY_ESCAPE): exit()
+	if Input.is_key_pressed(KEY_ESCAPE): go_back()
 	if Input.is_joy_button_pressed(0, JOY_XBOX_A) or Input.is_key_pressed(KEY_RETURN):
-		if self.menu_selected == MENU_START: choose_map()
-		if self.menu_selected == MENU_HISCORE: show_hiscore()
-		if self.menu_selected == MENU_EXIT: exit()
+		if self.menu_selected == MENU_GRASS: start_game()
+		if self.menu_selected == MENU_WATER: start_game()
+		if self.menu_selected == MENU_BACK: go_back()
 
 func _on_menu_button_selected( button_idx ):
 	self.menu_selected = button_idx
@@ -24,9 +24,8 @@ func _on_menu_button_selected( button_idx ):
 func show_hiscore():
 	pass
 
-func choose_map():
-	get_tree().change_scene("res://scenes/choose_map.tscn")
+func start_game():
+	get_tree().change_scene("res://scenes/game.tscn")
 
-func exit():
-	get_tree().quit()
-
+func go_back():
+	get_tree().change_scene("res://scenes/menu.tscn")
